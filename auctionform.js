c45e3a09abb9626a0b8e32efc94ca4bfc9877d2c
AIRO2K24 <airo.4.0.2024@gmail.com>
2024-08-13 15:08:00 +0530

    Update

--- auctionform.js
+++ auctionform.js
@@ -1,11 +1,16 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxAqOhICG8J4_BiHmBThSbSLJ-QfqiLwjDtCypKSgCzHJaTqKIL0WS7FPHgvzuZVsLX/exec'
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxAqOhICG8J4_BiHmBThSbSLJ-QfqiLwjDtCypKSgCzHJaTqKIL0WS7FPHgvzuZVsLX/exec";
 
-const form = document.forms['contact-form']
+const form = document.forms["contact-form"];
 
-form.addEventListener('submit', e => {
- e.preventDefault()
- fetch(scriptURL, { method: 'POST', body: new FormData(form)})
- .then(response => alert("Thank you! your form is submitted successfully." ))
- .then(() => { window.location.reload(); })
- .catch(error => console.error('Error!', error.message))
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) =>
+      alert("Thank you! your form is submitted successfully.")
+    )
+    .then(() => {
+      window.location.reload();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
--- coderform.js
+++ coderform.js
@@ -1,11 +1,16 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxMkmt6ILRAA216aqqdBakw9OuugV8GsLwoWWqD3m3SV62kk-Pv-YW34q01CnATx0fZmw/exec'
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxMkmt6ILRAA216aqqdBakw9OuugV8GsLwoWWqD3m3SV62kk-Pv-YW34q01CnATx0fZmw/exec";
 
-const form = document.forms['contact-form']
+const form = document.forms["contact-form"];
 
-form.addEventListener('submit', e => {
- e.preventDefault()
- fetch(scriptURL, { method: 'POST', body: new FormData(form)})
- .then(response => alert("Thank you! your form is submitted successfully." ))
- .then(() => { window.location.reload(); })
- .catch(error => console.error('Error!', error.message))
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) =>
+      alert("Thank you! your form is submitted successfully.")
+    )
+    .then(() => {
+      window.location.reload();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
