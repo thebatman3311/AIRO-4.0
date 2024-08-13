893df8dd87b30b12d3f00e021fb7c5ec404ad022
AIRO2K24 <airo.4.0.2024@gmail.com>
2024-08-13 15:02:24 +0530

    Update

--- infinityform.js
+++ infinityform.js
@@ -1,11 +1,16 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxi1ipIhO-yP3kipkvhKuKOsoOpmyyFI0WD6qZdpqROX7ANWjLZxpU9BC6MO_6vflQbQQ/exec'
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxi1ipIhO-yP3kipkvhKuKOsoOpmyyFI0WD6qZdpqROX7ANWjLZxpU9BC6MO_6vflQbQQ/exec";
 
-const form = document.forms['contact-form']
+const form = document.forms["contact-form"];
 
-form.addEventListener('submit', e => {
- e.preventDefault()
- fetch(scriptURL, { method: 'POST', body: new FormData(form)})
- .then(response => alert("Thank you! your form is submitted successfully." ))
- .then(() => { window.location.reload(); })
- .catch(error => console.error('Error!', error.message))
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) =>
+      alert("Thank you! your form is submitted successfully.")
+    )
+    .then(() => {
+      window.location.reload();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
