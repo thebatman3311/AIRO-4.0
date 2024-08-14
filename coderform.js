25f4275ba5964de38afa2dda6c2a098437a21af8
Praveen952k4 <praveenkalimuthu@gmail.com>
2024-08-14 15:00:10 +0530

    praveen

--- coderform.js
+++ coderform.js
@@ -7,7 +7,7 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
     .then((response) =>
-      alert("Thank you! your form is submitted successfully.")
+      window.location.href ='thank1.html'
     )
     .then(() => {
       window.location.reload();
--- form1.js
+++ form1.js
@@ -7,10 +7,26 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   fetch(scriptURL, { method: "POST", body: new FormData(form) })
     .then((response) =>
-      alert("Thank you! your form is submitted successfully.")
-    )
-    .then(() => {
-      window.location.reload();
-    })
+window.location.href ='thank6.html'    )
+
     .catch((error) => console.error("Error!", error.message));
 });
+
+// function handleSubmit(event) {
+//   // Get the email input field
+//   const emailInput = document.getElementById('email-input');
+//   const emailValue = emailInput.value.trim();
+  
+//   // Check if the email value contains '@gmail.com'
+//   if (!emailValue.endsWith('@gmail.com')) {
+//       // Show an error message
+//       alert('Please enter an email address ending with @gmail.com');
+//       // Prevent the default form submission
+//       event.preventDefault();
+//   } else {
+//       // Redirect to the new page
+//       window.location.href = 'thank6.html';
+//   }
+// }
+
+// document.getElementById('myForm').addEventListener('submit', handleSubmit);
